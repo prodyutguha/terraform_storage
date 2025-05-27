d9238dd32d017ef65b860d4a257cd4e3cf95cef3
John Doe <prodyut@kloudgurdian.com>
2025-05-27 14:50:05 +0530

    Storage account creation

--- main.tf
+++ main.tf
@@ -1,10 +1,16 @@
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "RG-Terraform-Storage-Account"
   location = "East US"
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "terraformstorage1990"     # must be globally unique
+  name                     = "tfstorage${random_string.suffix.result}"     # must be globally unique
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
