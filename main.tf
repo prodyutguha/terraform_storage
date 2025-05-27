8e5239980cdb602c66170fef5da7388ac11730fb
John Doe <prodyut@kloudgurdian.com>
2025-05-27 14:41:34 +0530

    Storage account creation

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "example" {
-  name     = "RG-Terraform"
+  name     = "RG-Terraform-Storage-Account"
   location = "East US"
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "terraformstorage"     # must be globally unique
+  name                     = "terraformstorage1990"     # must be globally unique
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
